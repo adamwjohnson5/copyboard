f6bbaf9cfb26bff54dbc91ae171a696241cc9486
Adam Johnson <adamjohnson@apple.com>
2019-04-24 22:42:18 +1000

    Update copyboard.js

--- copyboard.js
+++ copyboard.js
@@ -1,19 +1,22 @@
-/* Ripley
-Simple CSS animation via JavaScript.
-https://github.com/adamwjohnson5/ripley
+/* Copyboard
+Easy JS copy to clipboard.
+https://github.com/adamwjohnson5/copyboard
 By Adam Johnson
 MIT License 2019 */
 "use strict";
 
-class CopyClip {
+class Copyboard {
     copy(string) {
+        // Create input
         let i = document.createElement('input');
         i.setAttribute('id', 'copyToClipboard');
         document.body.appendChild(i);
-        let j = document.querySelector('input#copyToClipboard')
+        // Get input and add value
+        let j = document.querySelector('input#copyToClipboard');
         j.style.opacity = 0;
         j.value = string;
         j.select();
+        // Copy and remove
         document.execCommand('copy');
         j.remove();
     }
