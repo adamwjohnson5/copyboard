70dcd956a715ddc5862a4726dc1a64ea7efbd078
Adam Johnson <adamjohnson@apple.com>
2020-06-09 16:18:32 -0700

    Update copyboard.js

--- copyboard.js
+++ copyboard.js
@@ -3,6 +3,7 @@ Easy JS copy to clipboard.
 https://github.com/adamwjohnson5/copyboard
 By Adam Johnson
 MIT License 2019 */
+
 "use strict";
 
 class Copyboard {
@@ -11,11 +12,13 @@ class Copyboard {
         let i = document.createElement('input');
         i.setAttribute('id', 'copyToClipboard');
         document.body.appendChild(i);
+        
         // Get input and add value
         let j = document.querySelector('input#copyToClipboard');
         j.style.opacity = 0;
         j.value = string;
         j.select();
+        
         // Copy and remove
         document.execCommand('copy');
         j.remove();
