45b75d7efc9f5aef6006a700be8a141bce488434
Adam Johnson <adamjohnson@apple.com>
2020-09-21 14:48:29 -0700

    Update copyboard.js

--- copyboard.js
+++ copyboard.js
@@ -10,23 +10,23 @@ MIT License 2019
 
 */
 
-"use strict";
+'use strict';
 
 class Copyboard {
-  static copy(string) {
-    // Create input
-    const input = document.createElement("input");
-    input.setAttribute("id", "copyToClipboard");
-    input.value = string;
-    input.style.opacity = 0;
-    document.querySelector("body").after(input);
+    static copy(string) {
+        // Create input
+        const input = document.createElement('input');
+        input.setAttribute('id', 'copyToClipboard');
+        input.value = string;
+        input.style.opacity = 0;
+        document.querySelector('body').after(input);
 
-    // Get input and select value
-    const copyToClipboard = document.querySelector("input#copyToClipboard");
-    copyToClipboard.select();
+        // Get input and select value
+        const copyToClipboard = document.querySelector('input#copyToClipboard');
+        copyToClipboard.select();
 
-    // Copy then remove
-    document.execCommand("copy");
-    copyToClipboard.remove();
-  }
+        // Copy then remove
+        document.execCommand('copy');
+        copyToClipboard.remove();
+    }
 }
